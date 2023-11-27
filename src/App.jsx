a56e057b8ec5e244e471a6af94e35cb7c6b449c0
ivxvi12 <iam@undermilli.com>
2023-11-27 21:56:01 +0900

    reset for ex.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom'; // Importing necessary components from react-router-dom
 import Login from "./pages/login";
 import Signin from "./pages/signin";
+import Ex from "./pages/ex";
 
 function App() {
   return (
     <Routes> 
       <Route path="/login" element={<Login />} /> 
       <Route path="/signin" element={<Signin />} /> 
+      <Route path="/ex" element={<Ex />} /> 
     </Routes>
   );
 }
