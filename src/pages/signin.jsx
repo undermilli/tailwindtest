a8de0b4e308681454407b88ec7b76950347ddd0c
Cho Yoongeon <tesung5470@naver.com>
2023-11-28 10:26:35 +0900

    LOL-38 style: signup modal layout

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,12 +1,163 @@
-import { useState } from 'react'
-import '../index.css'
+import { useState } from "react";
+import "../index.css";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
 
 function Signin() {
+  const OPTIONS = [
+    { value: "none", email: "메일 선택" },
+    { value: "naver", email: "naver.com" },
+    { value: "banana", email: "google.com" },
+    { value: "orange", email: "hanmail.net" },
+  ];
+
+  const SelectBox = (props) => {
+    return (
+      <select className=" w-1/2 bg-selectBoxColor text-selected text-lg outline-none rounded-xl border-2 border-inputBorder">
+        {props.options.map((option) => (
+          <option
+            key={option.value}
+            value={option.value}
+            defaultValue={props.defaultValue === option.value}
+          >
+            {option.email}
+          </option>
+        ))}
+      </select>
+    );
+  };
+
   return (
-    <div>
-        signin
+    <div className="w-1/3 h-3/5 bg-slate-800/60 flex flex-col items-center justify-center fixed inset-0 m-auto text-whiteF rounded-3xl">
+      <button className="w-8 h-8 text-3xl absolute top-10 left-14">x</button>
+      <div className="bg-logoGray w-12 h-12 rounded-full "></div>
+      <h1 className="text-3xl">회원가입</h1>
+      <div>
+        <div className="m-2.5 w-96">
+          <label
+            htmlFor="nickname"
+            className="block text-sm font-medium leading-6 text-teal-500"
+          >
+            리더보드에서 사용할 닉네임
+          </label>
+          <div className="mt-0">
+            <input
+              type="text"
+              name="nickname"
+              id="nickname"
+              className="block w-full rounded-md border-0 px-1.5 py-1.5 shadow-sm ring-1 ring-inset ring-teal-500 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-teal-500 bg-transparent text-whiteF"
+              placeholder="사용할 닉네임"
+            />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="m-2.5 w-96">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium leading-6 text-teal-500"
+          >
+            E-mail 주소
+          </label>
+          <div className="mt-0">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              className="block w-full rounded-md border-0 px-1.5 py-1.5  shadow-sm ring-1 ring-inset ring-teal-500 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-teal-500  bg-transparent text-whiteF"
+              placeholder="you@example.com"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="m-2.5 w-96">
+        <label
+          htmlFor="password"
+          className="flex text-sm justify-between font-medium leading-6 text-teal-500"
+        >
+          <p>비밀번호</p>
+          <div className="flex items-center">
+            <EyeSlashIcon className="h-4 w-4 mx-1 text-whiteF" />
+            <p className="mx-1 text-whiteF">비밀번호 보이지 않기</p>
+          </div>
+        </label>
+        <div className="relative mt-0 rounded-md shadow-sm">
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className="block w-full rounded-md border-0 px-1.5 py-1.5 shadow-sm ring-1 ring-inset ring-teal-500 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-teal-500 bg-transparent text-whiteF"
+            placeholder="8자리 이상 입력해주세요."
+          />
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+            <EyeIcon className="h-5 w-5 text-whiteF" aria-hidden="true" />
+          </div>
+        </div>
+      </div>
+      <div className="m-2.5 w-96">
+        <label
+          htmlFor="verifyPassword"
+          className="flex text-sm justify-between font-medium leading-6 text-teal-500"
+        >
+          <p>비밀번호 확인</p>
+          <div className="flex items-center">
+            <EyeSlashIcon className="h-4 w-4 mx-1 text-whiteF" />
+            <p className="mx-1 text-whiteF">비밀번호 보이지 않기</p>
+          </div>
+        </label>
+        <div className="relative mt-0 rounded-md shadow-sm">
+          <input
+            type="password"
+            name="verifyPassword"
+            id="verifyPassword"
+            className="block w-full rounded-md border-0 px-1.5 py-1.5 shadow-sm ring-1 ring-inset ring-teal-500 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-teal-500 bg-transparent text-whiteF"
+            placeholder="비밀번호 재입력"
+          />
+          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3">
+            <EyeIcon className="h-5 w-5 text-whiteF" aria-hidden="true" />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="text-left">
+          <div className="flex">
+            <input
+              id="comments"
+              aria-describedby="comments-description"
+              name="comments"
+              type="checkbox"
+              className="h-4 w-4 rounded border-teal-500 text-teal-500 focus:ring-teal-500"
+            />
+            <p className="text-sm">개인정보 수집 및 이용에 동의합니다.</p>
+          </div>
+          <a
+            href="localhost:5173"
+            className="text-teal-500 text-xs ml-7 underline"
+          >
+            약관 자세히 보기
+          </a>
+        </div>
+        <div className="text-left flex">
+          <input
+            id="comments"
+            aria-describedby="comments-description"
+            name="comments"
+            type="checkbox"
+            className="h-4 w-4 rounded border-teal-500 text-teal-500 focus:ring-teal-500"
+          />
+          <p className="text-sm">만 14세 이상입니다.</p>
+        </div>
+      </div>
+      <div className="m-10">
+        <button
+          type="button"
+          className="font-sans rounded-full bg-teal-800 px-20 py-3 text-base text-white shadow-sm hover:bg-teal-700"
+        >
+          가입하고 내 랭킹 확인하기
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Signin
+export default Signin;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 
-
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily:{
-        sans: ['Pretendard-Regular', 'sans'],
-      }
+      fontFamily: {
+        sans: ["Pretendard-Regular", "sans"],
+      },
+      colors: {
+        whiteF: "#ffffff",
+      },
+      backgroundColor: {
+        logoGray: "C4C4C4",
+      },
     },
   },
-  plugins: [require('@tailwindcss/forms')],
-}
-
+  plugins: [require("@tailwindcss/forms")],
+};
